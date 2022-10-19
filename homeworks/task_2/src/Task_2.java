c6ae348264ca47d8fa2c0bfe55da48718448c5f8
omezhka <murasakitta@gmail.com>
2022-10-19 17:07:22 +0300

    MESSAGE

--- homeworks/task_2/src/Task_2.java
+++ homeworks/task_2/src/Task_2.java
@@ -1,32 +1,64 @@
 public class Task_2 implements Task_2_base {
     @Override
     public int subtask_1_while(int num) {
-        // Найти максимальное число, являющееся полным квадратом,
-        // не превосходящее заданное натуральное num
-        return 0;
+        int n = 1;
+        int result = 1;
+        int k = 1;
+        while (result <= num) {
+            k = result;
+            result = n * n;
+            n++;
+        }
+        return k;
     }
 
     @Override
     public int subtask_2_while(int num) {
-        // Последовательность целых чисел p(n) определяется следующим образом:
-        // p(0) = 1
-        // p(k) = 2 * p(k - 1) + 6, k > 0
-        //Найти элемент последовательности с номером num
-        return 0;
+        int p = 1;
+        int p2 = 1;
+        int k = 0;
+        while (k != num) {
+            p = p2;
+            p2 = 2 * p + 6;
+            k++;
+        }
+        return p2;
     }
 
     @Override
     public boolean subtask_3_while(int num, int base) {
-        // Проверить, является ли число num натуральной степенью числа base
-        return false;
+        int a = 0;
+        int counter = base;
+        while (base < num) {
+            a = base;
+            base *= counter;
+        }
+        if (base == num) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public int subtask_4_while(int start, int end) {
-        // Вычислить, за какое минимальное число операций
-        // вычесть 1
-        // поделить на 2
-        // число start можно превратить в end. Гарантируется, что start >= end >= 1
-        return 0;
+        int counter = 0;
+        int k = start;
+        while (k != end) {
+            if (k % 2 > 0) {
+                k--;
+                counter++;
+
+            } else {
+                if (k / end >= 2) {
+                    k = k / 2;
+                    counter++;
+                } else {
+                    k--;
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 }
